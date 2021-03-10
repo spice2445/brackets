8ce42f7a2fd397b40264e6c07fca33778f9a27bb
spice2445 <79026145+spice2445@users.noreply.github.com>
2021-03-10 22:32:20 +0200

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let activeBrackets = []
+  let openBrackets = []
+  let closeBrackets = new Map()
+  for(const bracketConfig of bracketsConfig){
+    const openBracket = bracketConfig[0]
+    const closeBracket = bracketConfig[1]
+    openBrackets.push(openBracket)
+    closeBrackets[closeBracket] = openBracket
+  }
+  for(let bracket of str.split("")){
+    if(openBrackets.includes(bracket)){
+      if(closeBrackets[bracket] === bracket){
+        if(activeBrackets.includes(bracket)){
+          if(activeBrackets.pop() !== bracket){
+            return false
+            }
+          continue
+        }
+      }
+      activeBrackets.push(bracket)
+    }
+    else{
+      const openBracket = closeBrackets[bracket]
+      if(openBracket != activeBrackets.pop()){
+        return false
+      }
+    }
+  }
+  return !activeBrackets.length
 }
